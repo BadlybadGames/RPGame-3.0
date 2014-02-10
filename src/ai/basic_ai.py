e4229c7806db5dee42d496a1b7958bff5f7be71d
Sebsebeleb <seb@terminator.net>
2014-02-10 17:27:55 +0100

    AI Improvments + fixes

--- src/ai/basic_ai.py
+++ src/ai/basic_ai.py
@@ -1,40 +1,47 @@
 # -*- coding: utf-8 -*-
 import random
 from cocos import euclid
+from cocos.euclid import Vector2
+from ai.movement import MoveBasic
+
+import logging
+logger = logging.getLogger()
 
 from util import AttrDict
 
 class Ai(object):
 
     def __init__(self):
-        self.owner = -1
+        self.owner = None
         self.state = AttrDict()
 
     def update_ai(self):
         pass
 
     def _get_owner(self):
+        if not self.owner:
+            return None
+
         from game.game import game
         return game.get_entity(self.owner)
 
 class BasicEnemyAi(Ai):
     name = "BasicEnemyAi"
 
-    def __init__(self):
-        super(BasicEnemyAi, self).__init__()
-        self.new_destination()
-
     def update_ai(self):
+        from game.game import game
+
         owner = self._get_owner()
         if not owner:
             return
-            
-        owner.move_dir = (self.state.destination - owner.position).normalized()
-        
-        dv = self.state.destination - owner.position
-        if dv.magnitude() < 40:
-            self.new_destination()
 
-    def new_destination(self):
-        x, y = random.randint(50, 600), random.randint(50, 400)
-        self.state.destination = euclid.Vector2(x, y)
+        target = game.get_player()
+
+        if not target:
+            return
+
+        destination = MoveBasic.get_next(target)
+        v = Vector2(*destination) - Vector2(*owner.position)
+        v.normalize()
+
+        owner.move_dir = v
---
+++ src/ai/movement.py
@@ -0,0 +1,15 @@
+__author__ = 'Sebsebeleb'
+
+
+class MoveBasic(object):
+    """Ai that tries to move directly (confirming to the pathing) to its target"""
+
+
+    @staticmethod
+    def get_next(target):
+        """ Returns the next position to walk to
+
+
+        """
+
+        return target.position
