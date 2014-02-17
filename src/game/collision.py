927d1248023fecb11ffbafed25db63b7cf4207e0
Sebsebeleb <seb@terminator.net>
2014-02-17 16:30:52 +0100

    pathfinding algorithm runs, but not done.

--- src/game/collision.py
+++ src/game/collision.py
@@ -1,9 +1,109 @@
 # -*- coding: utf-8 -*-
+from itertools import product
+import cocos
+from cocos.euclid import Vector2
 
 import numpy as np
 import collections
 
-class Theta_star(object):
+
+### TODO: Remove this, just for reference
+class AStar(object):
+    def __init__(self, graph):
+        self.graph = graph
+
+    def heuristic(self, node, start, end):
+        raise NotImplementedError
+
+    def search(self, start, end):
+        openset = set()
+        closedset = set()
+        current = start
+        openset.add(current)
+        while openset:
+            current = min(openset, key=lambda o:o.g + o.h)
+            if current == end:
+                path = []
+                while current.parent:
+                    path.append(current)
+                    current = current.parent
+                path.append(current)
+                return path[::-1]
+            openset.remove(current)
+            closedset.add(current)
+            for node in self.graph[current]:
+                if node in closedset:
+                    continue
+                if node in openset:
+                    new_g = current.g + current.move_cost(node)
+                    if node.g > new_g:
+                        node.g = new_g
+                        node.parent = current
+                else:
+                    node.g = current.g + current.move_cost(node)
+                    node.h = self.heuristic(node, start, end)
+                    node.parent = current
+                    openset.add(node)
+        return None
+
+class AStarNode(object):
+    def __init__(self):
+        self.g = 0
+        self.h = 0
+        self.parent = None
+
+    def move_cost(self, other):
+        raise NotImplementedError
+
+####end reference
+
+class Node(object):
+    def __init__(self, x, y):
+        self.g = 0
+        self.h = 0
+        self.parent = None
+        self.x, self.y = x, y
+
+class ThetaStar(object):
+    """Implemtents theta*: http://aigamedev.com/open/tutorials/theta-star-any-angle-paths/
+
+    TODO: Use lazy theta* instead: http://aigamedev.com/open/tutorial/lazy-theta-star/
+    TODO: This should be a functional class
+    """
+
+    def __init__(self, graph):
+        self.graph = graph
+
+    def search(self, start, end):
+        open_set = set()
+        closed_set = set()
+        current = start
+        open_set.add(current)
+        while open_set:
+            current = min(open_set, key=lambda o: o.g + o.h)
+            if current == end:
+                path = []
+                while current.parent:
+                    path.append(current)
+                    current = current.parent
+                path.append(current)
+                return path[::-1]
+            open_set.remove(current)
+            closed_set.add(current)
+            for node in self.graph[current]:
+                if node in closed_set:
+                    continue
+                if node in open_set:
+                    new_g = current.g + self.c(current, node)
+                    if node.g > new_g:
+                        node.g = new_g
+                        node.parent = current
+                else:
+                    node.g = current.g + self.c(current, node)
+                    node.parent = current
+                    open_set.add(node)
+        return None
+
 
     @classmethod
     def line_of_sight(cls, s, s1):
@@ -55,59 +155,57 @@ class Theta_star(object):
     @classmethod
     def update_vertex(cls, s, s1):
         g_old = s1.g
-        ComputeCost(s, s1)
+        cls.compute_cost(s, s1)
         if s1.g < g_old:
-            if s1.g < g_old:
-                if s1 in open_list:
-                    open.remove(s1)
-                open.insert(s1, s1.g + s1.h)
+            if s1 in cls.open_list:
+                if s1 in cls.open_list:
+                    cls.open_list.remove(s1)
+                cls.open_list.insert(s1, s1.g + s1.h)
 
     @classmethod
     def compute_cost(cls, s, s1):
         if cls.line_of_sight(s.parent, s1):
             # Path 2
-            s1.parent = s.parent
-            s1.g = s.g + c(s, s1) # c? c=compute_cost?
+            if s.parent.g + cls.c(s.parent, s1) < s1.g:
+                s1.parent = s.parent
+                s1.g = s.parent.g + cls.c(s.parent, s1)
         else:
             # Path 1
-            if s.g + c(c, s1) < s1.g:
+            if s.g + cls.c(s, s1) < s1.g:
                 s1.parent = s
-                s1.g = s.g + c(s, s1)
+                s1.g = s.g + cls.c(s, s1)
 
-class Node(object):
-    def __init__(self):
-        self.cost = 1
-        self.weight = 9999
-        self.parent = None
+    @classmethod
+    def c(cls, s, s1):
+        """returns the length of straight line from vertex s to s1"""
+        return (Vector2(s.x, s.y) - Vector2(s1.x, s1.y)).magnitude() * 32  # this might be incorrect
+
+def make_graph(w, h):
+    w, h = w + 1, h + 1  # We are making a vertex graph out of a grid map
+    nodes = [[Node(x, y) for y in range(h)] for x in range(w)]
+    graph = {}
+    for x, y in product(range(w), range(h)):
+        node = nodes[x][y]
+        graph[node] = []
+        for i, j in product([-1, 0, 1], [-1, 0, 1]):
+            if not (0 <= x + i < w):
+                continue
+            if not (0 <= y + j < h):
+                continue
+            graph[nodes[x][y]].append(nodes[x+i][y+j])
+    return graph, nodes
+
 
 class Pathfinding(object):
 
     _cache = {}
 
-    def __init__(self, map):
+    def __init__(self, map, width, height):
         self.map = map
+        self.grid = np.zeros((width, height))
+        self.width = width
+        self.height = height
 
-    def _astar(self, start, goal):
-        map = [Node(n) for n in m for m in self.map]
-        open_list = collections.deque()
-        closed_list = []
-
-        open_list.append(map[start])
-
-        while open_list:
-            node = open_list.popleft()
-            if node == goal:
-                return find_path
-
-    def _lazy_theta(self, start, goal):
-        open_list = collections.deque()
-        closed_list = collections.deque()
-
-
-
-    def _smooth_path(self, path):
-        # First remove all consecutive movements with the same movement vector
-        # Then from remaining paths, remove redundant nodes so remaining nodes all have line of sight to each other
     def get_path(self, v1, v2):
         """Returns the path needed to take on the collision grid map from v1 to v2.
 
@@ -124,8 +222,11 @@ class Pathfinding(object):
             path = self._cache.get((v1, v2)) or reversed(self._cache.get((v2, v1)))
             return path
 
-        path = self._astar(v1, v2)
-        return self._smooth_path(path)
+        graph, nodes = make_graph(self.width, self.height)
+        t = ThetaStar(graph)
+        start, end = nodes[v1[0]][v1[1]], nodes[v2[0]][v2[1]]
+        path = t.search(start, end)
+        return path
 
     def clear(self):
         """ Should be called when the map is updated. Clears cache
@@ -137,7 +238,7 @@ class Pathfinding(object):
 class CollisionGrid(object):
 
     def __init__(self, tile_width=48, tile_height=48, width=20, height=20):
-        """
+        """Grid representation of the collision map
 
         @param tile_width: Width of one tile in pixels
         @param tile_height: Height of one tile in pixels
@@ -145,6 +246,8 @@ class CollisionGrid(object):
         @param height: Number of tiles in the height
         """
         self.grid = np.zeros((width, height))
+        mp = cocos.tiles.load_tmx("test.tmx")
+
 
     def show(self):
         import cocos
